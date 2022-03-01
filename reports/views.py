aeef1190fdd4e85be153516f76481811d9236045
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2022-03-01 01:03:38 -0300

    Create report object from form with Ajax

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from profiles.models import Profile
+
+from .utils import get_report_image
+from .models import Report
 
 # Create your views here.
+def create_report_view(request):
+
+    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+    if is_ajax:
+        name = request.POST.get('name')
+        remarks = request.POST.get('remarks')
+        image = get_report_image(request.POST.get('image'))
+        author = Profile.objects.get(user=request.user)
+
+        Report.objects.create(name=name, remarks=remarks, image=image, author=author)
+
+        return JsonResponse({'message': 'sent'})
+    return JsonResponse({})
--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -1,13 +1,44 @@
-
 const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
 const modalBody = document.getElementById('modal-body')
 
-// if (img) {
-//     document.getElementById('img').classList.remove('not-visible')
-// }
+const reportForm = document.getElementById('report-form')
+
+const reportName = document.getElementById('id_name')
+const reportRemarks = document.getElementById('id_remarks')
+const csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value
+
+if (img) {
+    document.getElementById('img').classList.remove('not-visible')
+}
 
 reportBtn.addEventListener('click', () => {
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
+
+    reportForm.addEventListener('submit', event => {
+        event.preventDefault()
+
+        const formData = new FormData()
+        formData.append('name', reportName.value)
+        formData.append('remarks', reportRemarks.value)
+        formData.append('image', img.src)
+
+        const request = new Request(
+            'reports/save/',
+            {
+                method: 'POST',
+                mode: 'same-origin',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'X-CSRFToken': csrfToken},
+                body: formData 
+            }
+        )
+
+        fetch(request).then(response => {
+            response.json().then(data => {
+                console.log(data)
+            })
+        })
+
+    })
 })
