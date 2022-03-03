0b75b735143d09edfa77b9bc430342314949a323
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2022-03-03 01:24:42 -0300

    Add alerts to the report modal

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -3,10 +3,19 @@ const img = document.getElementById('img')
 const modalBody = document.getElementById('modal-body')
 
 const reportForm = document.getElementById('report-form')
+const alertBox = document.getElementById('alert-box')
 
 const reportName = document.getElementById('id_name')
 const reportRemarks = document.getElementById('id_remarks')
 const csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value
+
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `
+}
 
 if (img) {
     document.getElementById('img').classList.remove('not-visible')
@@ -34,11 +43,19 @@ reportBtn.addEventListener('click', () => {
             }
         )
 
-        fetch(request).then(response => {
-            response.json().then(data => {
-                console.log(data)
+        fetch(request)
+            .then(response => {
+
+                if (!response.ok) {
+                    handleAlerts('danger', `Oops! Something went wrong! Status ${response.status}`)
+                    return;
+                }
+
+                response.json()
+                    .then(data => console.log(data))
+
+                handleAlerts('success', 'Report created!')    
             })
-        })
 
     })
 })
