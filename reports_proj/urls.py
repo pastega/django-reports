0e7d191e05f856dc117aaac32e9f3b5ded9f7120
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2022-03-04 21:36:42 -0300

    Add authentication

--- reports_proj/urls.py
+++ reports_proj/urls.py
@@ -18,12 +18,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import login_view, logout_view
+
 urlpatterns = [
     path('', include('sales.urls', namespace='sales')),
     path('admin/', admin.site.urls),
     
     path('reports/', include('reports.urls', namespace='reports')),
-    path('my-profile/', include('profiles.urls', namespace='profiles'))
+    path('my-profile/', include('profiles.urls', namespace='profiles')),
+
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ] 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
